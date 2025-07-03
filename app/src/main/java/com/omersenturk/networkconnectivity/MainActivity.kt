75ab042b8d05db6972aa77f5eff6c23f3e3e4020
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-03 22:24:32 +0300

    network connectivity listiner function created

---
+++ app/src/main/java/com/omersenturk/networkconnectivity/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.omersenturk.networkconnectivity
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import javax.security.auth.callback.Callback
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        checkNetwork()
+    }
+    private fun checkNetwork(){
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        networkCallback = object : ConnectivityManager.NetworkCallback(){
+            override fun onLost(network: Network) {
+                Toast.makeText(this@MainActivity, "Network Disconnected", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest,networkCallback)
+    }
+}
