6a017633be8513923b2c0fcb1641ee12443fae15
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-05 15:37:38 +0300

    Add Wi-Fi and cellular network detection to NetworkChangeReceiver

--- app/src/main/java/com/omersenturk/networkconnectivity/NetworkChangeReciver.kt
+++ app/src/main/java/com/omersenturk/networkconnectivity/NetworkChangeReciver.kt
@@ -13,15 +13,18 @@ class NetworkChangeReciver : BroadcastReceiver() {
             context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val network = connectivityManager.activeNetwork
         val capabilities = connectivityManager.getNetworkCapabilities(network)
-        val isConnected =
-            capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
 
-        val message = if (isConnected) {
-            "Connected Network"
-        } else {
-            "Network Disconnected"
+        if(capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true){
+            when{
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Toast.makeText(context, "Network Connected with Wi-Fi", Toast.LENGTH_SHORT).show()
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Toast.makeText(context, "Network Connected with Cellular", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }else{
+            Toast.makeText(context, "Network Disconnected", Toast.LENGTH_SHORT).show()
         }
-
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
