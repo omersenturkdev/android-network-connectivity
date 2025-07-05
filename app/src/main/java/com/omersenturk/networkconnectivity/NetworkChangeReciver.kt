e148d6728dc6d6e653898ed171cad08f9cf7df82
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-05 22:41:59 +0300

    fix NetworkChangeReciver.kt

--- app/src/main/java/com/omersenturk/networkconnectivity/NetworkChangeReciver.kt
+++ app/src/main/java/com/omersenturk/networkconnectivity/NetworkChangeReciver.kt
@@ -9,22 +9,30 @@ import android.widget.Toast
 
 class NetworkChangeReciver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val connectivityManager =
-            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork
-        val capabilities = connectivityManager.getNetworkCapabilities(network)
+        if (intent?.action == ConnectivityManager.CONNECTIVITY_ACTION) {
+            val connectivityManager =
+                context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val network = connectivityManager.activeNetwork
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
 
-        if(capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true){
-            when{
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                    Toast.makeText(context, "Network Connected with Wi-Fi", Toast.LENGTH_SHORT).show()
+            if (capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        Toast.makeText(context, "Network Connected with Wi-Fi", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        Toast.makeText(
+                            context,
+                            "Network Connected with Cellular",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                    Toast.makeText(context, "Network Connected with Cellular", Toast.LENGTH_SHORT).show()
-                }
+            } else {
+                Toast.makeText(context, "Network Disconnected", Toast.LENGTH_SHORT).show()
             }
-        }else{
-            Toast.makeText(context, "Network Disconnected", Toast.LENGTH_SHORT).show()
         }
     }
 }
